f721dbcd899583f58167b74c58d86fdafd9c486d
Joel Grant <jgrant94@jg-mbook.ncat.edu>
2017-04-19 22:05:39 -0400

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var secret = require('./config/secret');
 var User = require('./models/user');
 var Category = require('./models/category');
 
-var middleware = require('./middlewares/middleware');
+var cartLength = require('./middlewares/middlewares');
 var app = express();
 
 mongoose.connect(secret.database, function(err){
@@ -56,6 +56,9 @@ app.use(function(req, res, next) {
 	res.locals.user = req.user;
 	next();
 });
+
+
+app.use(cartLength)
 
 app.use(function(req, res, next) {
 	//find all categories
